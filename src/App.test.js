7bac6d3d938b511157761f1e276868aea467232f
Lucas Baker <luke.baker7@gmail.com>
2016-11-21 10:10:29 -0800

    Did some of the stuff in class. Not finished.

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,22 @@
 import React from 'react';
+import {shallow} from 'enzyme';
+import {DogList} from './PetApp';
+
 
 //tests go here!
+describe('Basic math and logic', () => {
+    it('should confirm that 1+1=2', () => {
+        expect(1+1).toEqual(2);
+    });
+
+    it('should not contain my name', () => {
+        expect(['Joel', 'Luis', 'Cameron']).not.toContain('Luke');
+    })
+});
+
+describe('<App> component', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<DogList />);
+    })
+});
+
